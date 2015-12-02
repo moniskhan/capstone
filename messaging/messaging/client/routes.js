3ca1b4f4d1e1e2b17890fbd37713c357ea12aff6
oscarleung <oscar-who@hotmail.com>
2015-12-01 20:29:58 -0500

    UI and landing page, dont force push plz ty bb

--- messaging/messaging/client/routes.js
+++ messaging/messaging/client/routes.js
@@ -1,12 +1,13 @@
-Router.configure({
-  layoutTemplate: 'app'
-});
-
 Router.route('/:channel', function () {
 	Session.set('channel', this.params.channel);
+	this.layout('app');
 	this.render('messages');
 });
 
-Router.route('/', function () {
-	this.redirect('/general');
+
+Router.route('/', function() {
+	this.layout('landingTemplate');
+	this.render('landing');
+	
+	
 });
